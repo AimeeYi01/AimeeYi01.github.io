b4d6408ec7b276b6790b967fa2519c38c5b24deb
AimeeYi01 <yix266@newschool.edu>
2023-12-03 17:30:23 -0500

    e9

--- entries/entry9/main.js
+++ entries/entry9/main.js
@@ -1,7 +1,7 @@
-function showVideoModal() {
-  document.getElementById('video-modal').style.display = 'flex';
+function showvideo() {
+  document.getElementById('video').style.display = 'flex';
 }
 
-function hideVideoModal() {
-  document.getElementById('video-modal').style.display = 'none';
+function hidevideo() {
+  document.getElementById('video').style.display = 'none';
 }
