3a5635c0ae877a4998efd980f32668bf6b483723
AimeeYi01 <yix266@newschool.edu>
2023-12-03 14:25:59 -0500

    e10

--- entries/entry10/main.js
+++ entries/entry10/main.js
@@ -1,9 +1,9 @@
 let currentIndex = 0;
-    const totalImages = document.querySelectorAll('.slide').length;
-    const slideshow = document.getElementById('slideshow');
+    const totalimages = document.querySelectorAll('.slide').length;
+    const slides = document.getElementById('slides');
 
-function toggleImage() {
-    currentIndex = (currentIndex + 1) % totalImages;
+function toggleimage() {
+    currentIndex = (currentIndex + 1) % totalimages;
     const translateValue = -currentIndex * 100 + '%';
-    slideshow.style.transform = 'translateX(' + translateValue + ')';
+    slides.style.transform = 'translateX(' + translateValue + ')';
 }
