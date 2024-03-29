25cf58a246bd6a5643f960d6251e845f2d8b246c
AimeeYi01 <yix266@newschool.edu>
2024-03-29 09:35:14 -0400

    a

--- archivehomepage/script.js
+++ archivehomepage/script.js
@@ -19,22 +19,22 @@ document.getElementById("t5").addEventListener("click", function() {
   });
 
 
-// 获取按钮和弹窗元素
+
 const aboutBtn = document.getElementById('aboutBtn');
 const popup = document.getElementById('popup');
 const closeBtn = document.getElementById('closeBtn');
 
-// 点击 About 按钮显示弹窗
+
 aboutBtn.addEventListener('click', () => {
   popup.style.display = 'block';
 });
 
-// 点击关闭按钮隐藏弹窗
+
 closeBtn.addEventListener('click', () => {
   popup.style.display = 'none';
 });
 
-// 点击弹窗外部隐藏弹窗（可选）
+
 window.addEventListener('click', (event) => {
   if (event.target === popup) {
     popup.style.display = 'none';
