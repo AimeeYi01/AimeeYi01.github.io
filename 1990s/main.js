42b335dc48298e84585dae11e575d5ccc8e14827
AimeeYi01 <yix266@newschool.edu>
2024-03-29 09:39:10 -0400

    a

--- 1990s/main.js
+++ 1990s/main.js
@@ -1,19 +1,18 @@
 console.log("Start loading JSON data...");
 
 $(document).ready(function() {
-  // Load JSON data
+
   $.getJSON('data.json', function(data) {
-    // Iterate through each entry in the JSON data
+ 
     $.each(data, function(index, entry) {
-      // Create a container for each entry
+ 
       var container = $('<div class="container"></div>');
 
-      // Populate container with data
+ 
       container.append('<img src="' + entry.image + '" alt="Image">');
       container.append('<div class="content"><h2>' + entry.name + '</h2><p>' + entry.location + '</p><p>' + entry.date + '</p></div>');
       container.append('<div class="background-story-wrapper"><div class="background-story"><p>' + entry.background + '</p></div></div>');
 
-      // Append container to the page
       $('#container').append(container);
     });
   })
