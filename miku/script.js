6027f9153d100ffbbd9e98fdde571d4088ad13ee
AimeeYi01 <yix266@newschool.edu>
2024-04-20 21:45:50 -0400

    n

--- miku/script.js
+++ miku/script.js
@@ -1,23 +1,24 @@
-
-   
     const aboutBtn = document.getElementById("aboutbtn");
     const modal = document.getElementById("aboutmodal");
     const closeBtn = document.querySelector(".close");
 
-  
     aboutBtn.addEventListener("click", () => {
         modal.style.display = "block";
     });
 
-    
     closeBtn.addEventListener("click", () => {
         modal.style.display = "none";
     });
 
-    
     window.addEventListener("click", (event) => {
         if (event.target === modal) {
             modal.style.display = "none";
         }
     });
+
+    window.addEventListener('load', () => {
+        const img = document.querySelector('img');
+        img.style.transform = 'translateX(0)';
+    });
+
 
